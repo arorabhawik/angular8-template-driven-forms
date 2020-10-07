5692d952812551c4c2037e69e818eb0265dd1fa3
Bhawik <72097871+arorabhawik@users.noreply.github.com>
2020-10-07 12:44:02 +0530

    Angular 8 template driven forms sandbox

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f', {static: true}) signupForm: NgForm;
+
+  defaultValue = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
+  public suggestUserName() {
+    this.signupForm.setValue({
+      username: 'sample',
+      email: '',
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    });
+  }
+
+  public onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.username;
+    this.user.email = this.signupForm.value.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.onReset();
+  }
+
+  public onReset() {
+    this.signupForm.reset();
+  }
+}
